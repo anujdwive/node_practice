53cd4e4b082f6937290562dc027b77b6296174be
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-09-20 21:28:17 +0530

    add pagination and sort qurery

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,11 +7,51 @@ const router = express.Router();
 
 router.get("/", userAuth, validateObjectId, async (req, res, next) => {
   try {
-    const users = await User.find().select("-password");
+    // const { page, limit } = req.params;
+    const page = Number(req.query.page);
+    const limit = Number(req.query.limit);
+    const sortedOrder = req.query.order === desc ? -1 : 1;
+    const allowedField = ["createdAt", "firstName", "lastName"];
+    const sortedField = req.query.sort || "createdAt";
+
+    if (!allowedField.includes(sortedField)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Sort Field",
+      });
+    }
+
+    if (!Number.isInteger(page) || page < 1) {
+      return res.status(400).json({
+        success: false,
+        message: "Page must be a positive integer",
+      });
+    }
+
+    if (!Number.isInteger(limit) || limit < 1 || limit > 100) {
+      return res.status(400).json({
+        success: false,
+        message: "Limit must be an integer between 1 and 100",
+      });
+    }
+    const skip = (page - 1) * limit;
+    const users = await User.find()
+      .select("-password")
+      .sort({ [sortedField]: sortedOrder })
+      .skip(skip)
+      .limit(limit);
+    const total = await User.countDocuments();
+    const totalPages = Math.ceil(total / limit);
 
     return res.status(200).json({
       success: true,
       result: users,
+      pagination: {
+        page,
+        limit,
+        total,
+        totalPages,
+      },
     });
   } catch (error) {
     next(error);
