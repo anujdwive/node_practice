a10309f6dc52ba31bd7b5ed19d9f5404076ebf06
Anuj Dwivedi <anujdwivedi0705@gmail.com>
2026-09-24 21:55:41 +0530

    use indexing

--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,52 @@
-const { default: mongoose, model } = require("mongoose");
+const mongoose = require("mongoose");
 
-const userShema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user",
-  },
+);
+
+// 1. Email par unique index
+userSchema.index({ email: 1 }, { unique: true });
+
+// 2. Compound index
+userSchema.index({
+  role: 1,
+  createdAt: -1,
 });
 
-const User = mongoose.model("User", userShema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
